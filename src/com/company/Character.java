487f5ed455036c26c11611564d86214bcce6b940
Paul <p.a.m.van.berkel@gmail.com>
2020-02-09 19:46:11 +0100

    created character classes and weapons

---
+++ src/com/company/Character.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Character {
+
+    private String name;
+    private Integer health;
+    private Integer hitpoints;
+
+
+    public Character(String name, Integer health, Integer hitpoints) {
+        this.name=name;
+        this.health=health;
+        this.hitpoints=hitpoints;
+
+    }
+
+
+}
---
+++ src/com/company/Weapon.java
@@ -0,0 +1,18 @@
+package com.company;
+
+
+public class Weapon {
+
+    private String name;
+    private Integer hitpoints;
+    private Integer damaged;
+
+    public Weapon(String name, Integer hitpoints, Integer damaged) {
+
+        this.name=name;
+        this.hitpoints=hitpoints;
+        this.damaged=0;
+    }
+
+
+}
